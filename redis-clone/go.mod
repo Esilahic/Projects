7379c7d4e1e7b5f14d3c960f76988905f5c22b36
Esilahic <silahicemil11@gmail.com>
2023-09-23 16:26:21 -0600

    aa

---
+++ redis-clone/go.mod
@@ -0,0 +1,3 @@
+module github.com/Esilahic/Projects/Redis-clone
+
+go 1.21.1
--- redis-clone/main.go
+++ redis-clone/main.go
@@ -1,9 +1,51 @@
+//
+
 package main
 
 import (
 	"fmt"
+	"io"
+	"net"
+	"os"
 )
 
 func main() {
-	fmt.Println("Hello")
+	fmt.Println("Listening for port :6379")
+
+	// listen for port 6379 return err if unsuccessful
+	l, err := net.Listen("tcp", ":6379")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	// accept connection from port 6379 return err if unsuccessful
+	conn, err := l.Accept()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	// closing connection
+	defer conn.Close()
+
+	// infinite loop to read client msg, then respond
+	for {
+		buf := make([]byte, 1024)
+
+		// reading client message
+		_, err := conn.Read(buf)
+		if err != nil {
+			// if error is true then we reach end of file and terminate loop
+			if err == io.EOF {
+				break
+			}
+			// otherwise print error and exit with status 1
+			fmt.Println("error reading command: ", err.Error())
+			os.Exit(1)
+		}
+
+		// ignore request and return PONG
+		conn.Write([]byte("+OK\r\n"))
+	}
 }
