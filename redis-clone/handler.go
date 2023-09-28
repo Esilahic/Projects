50b63b99b25a52519d6d59f25461bc986e99ac2e
Esilahic <silahicemil11@gmail.com>
2023-09-27 21:22:08 -0600

    starting persistance, done hset and hget come back l8r

--- redis-clone/handler.go
+++ redis-clone/handler.go
@@ -6,6 +6,8 @@ var Handlers = map[string]func([]Value) Value{
 	"PING": ping,
 	"SET":  set,
 	"GET":  get,
+	"HSET": hset,
+	"HGET": hget,
 }
 
 func ping(args []Value) Value {
@@ -50,4 +52,46 @@ func get(args []Value) Value {
 	}
 
 	return Value{typ: "bulk", bulk: val}
+}
+
+// hash map within a hashmap for a json style format
+var HSETs = map[string]map[string]string{}
+var HSETsMu = sync.RWMutex{}
+
+func hset(args []Value) Value {
+	if len(args) != 3 {
+		return Value{typ: "error", str: "ERR wrong number of arguments for HSET command"}
+	}
+
+	hash := args[0].bulk
+	key := args[1].bulk
+	val := args[2].bulk
+
+	HSETsMu.Lock()
+	if _, ok := HSETs[hash]; !ok {
+		HSETs[hash] = map[string]string{}
+	}
+	HSETs[hash][key] = val
+	HSETsMu.Unlock()
+
+	return Value{typ: "string", str: "OK"}
+}
+
+func hget(args []Value) Value {
+	if len(args) != 2 {
+		return Value{typ: "error", str: "ERR wrong number of arguments for HGET command"}
+	}
+
+	hash := args[0].bulk
+	key := args[1].bulk
+
+	HSETsMu.Lock()
+	value, ok := HSETs[hash][key]
+	HSETsMu.Unlock()
+
+	if !ok {
+		return Value{typ: "null"}
+	}
+
+	return Value{typ: "bulk", bulk: value}
 }
