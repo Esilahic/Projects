688ea30238e6ac825e810921facb1fbfcd60e299
Esilahic <silahicemil11@gmail.com>
2023-09-27 22:23:01 -0600

    barebones done

--- go-laravel/celeritas/celeritas.go
+++ go-laravel/celeritas/celeritas.go
@@ -1,9 +1,1 @@
 package celeritas
-
-func TestFuncn(a, b int) int {
-	return a + b
-}
-
-func TestFunc2(a, b int) int {
-	return a - b
-}
--- go-laravel/myapp/main.go
+++ go-laravel/myapp/main.go
@@ -1,16 +1,5 @@
 package main
 
-import (
-	"fmt"
-
-	"github.com/Esilahic/Projects/go-laravel/celeritas"
-)
-
 func main() {
 
-	result := celeritas.TestFuncn(1, 1)
-	fmt.Println(result)
-
-	result = celeritas.TestFunc2(2, 1)
-	fmt.Println(result)
 }
