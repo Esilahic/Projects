2cbb218e13e2849d42a1a2fb4e5e7e81b39858fa
Esilahic <silahicemil11@gmail.com>
2023-10-01 18:20:56 -0600

    continuing work on laravel clone

--- go-laravel/celeritas/celeritas.go
+++ go-laravel/celeritas/celeritas.go
@@ -8,6 +8,28 @@ type Celeritas struct {
 	Version string
 }
 
-func (c *Celeritas) New(filePath string) error {
+func (c *Celeritas) New(rootPath string) error {
+	pathConfig := initPaths{
+		rootPath:    rootPath,
+		folderNames: []string{"handlers", "migrations", "views", "data", "public", "tmp", "logs", "middleware"},
+	}
 
+	err := c.Init(pathConfig)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (c *Celeritas) Init(p initPaths) error {
+	root := p.rootPath
+
+	for _, path := range p.folderNames {
+		// create folder if doesnt exist
+		err := c.CreateDirIfNotExist(root + "/" + path)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
 }
