395b4d656645840cfd1b43b7f27bf6045d202392
Esilahic <silahicemil11@gmail.com>
2023-10-04 21:19:55 -0600

    create renderer and started switch statement depending on customer using jet or default. added init render in celeritas.go and updated routes.go to remove boiler "hello"

--- go-laravel/celeritas/celeritas.go
+++ go-laravel/celeritas/celeritas.go
@@ -8,6 +8,7 @@ import (
 	"strconv"
 	"time"
 
+	"github.com/Esilahic/Projects/go-laravel/render"
 	"github.com/go-chi/chi/v5"
 	"github.com/joho/godotenv"
 )
@@ -22,6 +23,7 @@ type Celeritas struct {
 	InfoLog  *log.Logger
 	RootPath string
 	Routes   *chi.Mux
+	Render   *render.Render
 	config   config
 }
 
@@ -65,6 +67,8 @@ func (c *Celeritas) New(rootPath string) error {
 		port:     os.Getenv("PORT"),
 		renderer: os.Getenv("RENDERER"),
 	}
+
+	c.Render = c.CreateRenderer(c)
 
 	return nil
 }
@@ -114,4 +118,14 @@ func (c *Celeritas) startLoggers() (*log.Logger, *log.Logger) {
 	errorLog = log.New(os.Stdout, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
 
 	return infoLog, errorLog
+}
+
+func (c *Celeritas) CreateRenderer(cel *Celeritas) *render.Render {
+	myRenderer := render.Render{
+		Renderer: cel.config.renderer,
+		RootPath: cel.RootPath,
+		Port:     cel.config.port,
+	}
+
+	return &myRenderer
 }
