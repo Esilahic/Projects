02f4be351d720bc1caab33633949cc3c5a2537a1
Esilahic <silahicemil11@gmail.com>
2023-10-09 20:53:44 -0600

    extended tests to cover jet and go page funcs and added make file for tests

--- go-laravel/celeritas/render/render_test.go
+++ go-laravel/celeritas/render/render_test.go
@@ -22,9 +22,58 @@ func TestRender_Page(t *testing.T) {
 		t.Error("error rendering page", err)
 	}
 
+	err = testRenderer.Page(w, r, "no-file", nil, nil)
+	if err == nil {
+		t.Error("error rendering non-existant template", err)
+	}
+
 	testRenderer.Renderer = "jet"
+	err = testRenderer.Page(w, r, "home", nil, nil)
+	if err != nil {
+		t.Error("error rendering jet page", err)
+	}
+
+	err = testRenderer.Page(w, r, "no-file", nil, nil)
+	if err == nil {
+		t.Error("error rendering non-existant jet template", err)
+	}
+
+	testRenderer.Renderer = ""
+	err = testRenderer.Page(w, r, "home", nil, nil)
+	if err == nil {
+		t.Error("ERROR Invalid renderer specified", err)
+	}
+}
+
+func TestRender_GoPage(t *testing.T) {
+	w := httptest.NewRecorder()
+	r, err := http.NewRequest("GET", "anyurl", nil)
+	if err != nil {
+		t.Error(err)
+	}
+
+	testRenderer.Renderer = "go"
+	testRenderer.RootPath = "./testdata"
+
 	err = testRenderer.Page(w, r, "home", nil, nil)
 	if err != nil {
 		t.Error("error rendering page", err)
 	}
+
+}
+
+func TestRender_JetPage(t *testing.T) {
+	w := httptest.NewRecorder()
+	r, err := http.NewRequest("GET", "anyurl", nil)
+	if err != nil {
+		t.Error(err)
+	}
+
+	testRenderer.Renderer = "jet"
+
+	err = testRenderer.Page(w, r, "home", nil, nil)
+	if err != nil {
+		t.Error("error rendering jet page", err)
+	}
+
 }
